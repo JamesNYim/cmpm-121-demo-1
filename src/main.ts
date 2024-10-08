9cdb93adae950da11c193df2e7133ad59923a898
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-10-08 11:38:32 -0700

    Step One Complete: Added Button

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Create Button
+const button = document.createElement('button');
+button.textContent = '🌿';
+button.style.fontSize = '32px';
+button.className = 'plantButton';
+document.body.appendChild(button);
